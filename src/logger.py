bbe2151514c14576a02025c64ab2df05d30cda08
Muhammad Umer <umernadeem241@gmail.com>
2024-09-07 11:55:50 -0700

    logger

--- src/logger.py
+++ src/logger.py
@@ -4,8 +4,9 @@ from datetime import datetime
 
 
 Log_file= f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log()"
-log_path= os.path.join(os.getcwd(), "logs", Log_file)
 
+project_root = os.path.dirname(os.getcwd())
+log_path = os.path.join(project_root, "logs", Log_file)
 os.makedirs(log_path, exist_ok=True)
 
 LOG_FILE_PATH= os.path.join(log_path, Log_file)
